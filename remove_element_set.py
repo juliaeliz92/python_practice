9a78e00b66ad6494c53a7e49b52a69f1964e1d97
Julia Paul <juliapaulmyla@yahoo.com>
2024-01-30 19:13:47 -0500

    program to remove element from set

---
+++ remove_element_set.py
@@ -0,0 +1,11 @@
+# Create a program to remove a specific element from a set.
+
+my_set = {"apple", "orange", "banana"}
+
+print(f'set before removal {my_set}')
+
+print('removing banana from the set')
+
+my_set.remove('banana')
+
+print(f'set after removal {my_set}')
