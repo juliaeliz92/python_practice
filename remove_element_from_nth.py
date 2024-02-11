00202c2f233291cc0849e7c3ea5f7f4a99b29d19
Julia Paul <juliapaulmyla@yahoo.com>
2024-02-11 00:10:42 -0500

    day 26 - day 32

---
+++ remove_element_from_nth.py
@@ -0,0 +1,7 @@
+#Create a program that removes the nth element from a list.
+
+list1 = ['orange', 'banana', 'apple']
+
+list1.pop(2)
+
+print(list1)
---
+++ sort_list_aplhabetically.py
@@ -0,0 +1,7 @@
+#Create a program that sorts a list of strings alphabetically
+
+list1 = ['orange', 'banana', 'apple']
+
+list1.sort()
+
+print(list1)
